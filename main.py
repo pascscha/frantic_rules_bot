51a0203dc90b2ef2d8549bab47359e1e894dde26
Pascal Schärli <pascscha@student.ethz.ch>
2019-05-13 14:41:21 +0200

    Event generator

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
+import random
 
 
 def start(bot, update):
@@ -58,6 +59,53 @@ def pdf(bot, update):
                       caption="Here you go :)")
 
 
+def event(bot, update):
+    try:
+        events = ["Tornado", "Earthquake", "Finish Line", "Vandalism", "Doomsday", "Mating Season", "Robin Hood", "Surprise Party", "Gambling Man", "Time Bomb", "Communism", "Charity", "Friday the 13th", "Expansion", "Recession", "The All Seeing Eye", "Mexican Standoff", "Market", "Third Time Lucky", "Merry Christmas", "Russian Roulette", "Plague", "Event Manager", "Trust Fall", "Double Taxation", "Crowdfunding", "Identity Theft", "Last Chance", "Repeat", "Plus One", "Distributor", "Capitalism", "Seppuku", "Black Hole"]
+        query = update.message.text.lower()
+        if query == "/event":
+            with open("generated.txt") as f:
+                text = f.read().strip()
+            if len(text) == 0:
+                already_played = []
+            else:
+                already_played = [int(s) for s in text.split(",")]
+            unplayed = []
+            for i in range(len(events)):
+                if i + 1 not in already_played:
+                    unplayed.append(i + 1)
+            if len(unplayed) == 0:
+                bot.send_message(update.message.from_user.id,
+                                 text="All event cards have been played. Reshuffle with <code>/event shuffle</code>",
+                                 parse_mode=telegram.ParseMode.HTML)
+            else:
+
+                with open("raw_rules.txt") as f:
+                    raw = f.read().split("\n\n")
+                rules = {}
+                for r in raw:
+                    title, rule = r.split("\n")
+                    rules[title.lower()] = rule
+
+                choice = random.choice(unplayed)
+                url = "http://rulefactory.ch/special/troublemaker-generator/img/event{}.png".format(choice)
+                result = process.extractOne(events[choice - 1], list(rules.keys()), scorer=fuzz.ratio)[0]
+                explanation = rules[result]
+                bot.send_photo(update.message.from_user.id,
+                               photo=url,
+                               caption=explanation,
+                               parse_mode=telegram.ParseMode.HTML)
+                already_played.append(choice)
+
+                with open("generated.txt", "w+") as f:
+                    f.write(",".join([str(i) for i in already_played]))
+        elif query == "/event shuffle":
+            with open("generated.txt", "w+") as f:
+                f.write("")
+    except Exception as e:
+        print("Error:", e)
+
+
 # This is good practice: this code only gets run when we run DecidoBot.py,
 # but it does not get executed if we were to import some functions from it
 if __name__ == "__main__":
@@ -68,4 +116,5 @@ if __name__ == "__main__":
     dispatcher.add_handler(CommandHandler('rule', rule))
     dispatcher.add_handler(CommandHandler('pdf', pdf))
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('event', event))
     updater.start_polling()
